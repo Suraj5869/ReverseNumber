1562d1ac41797d6f6fc674fa7a0f7b6a90f4990d
unknown <suraj.sakhare5869@gmail.com>
2024-09-25 19:39:32 +0530

    First Commit

---
+++ ReversedNumber/Models/Reverse.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedNumber.Models
+{
+    internal class Reverse
+    {
+        public static int ReverseNumber(int number)
+        {
+            int divisor = 10;
+            int temp = number;
+            int output = 0;
+
+            while (temp > 0)
+            {
+                int digit = temp % divisor;
+                temp = temp / divisor;
+                output = output * 10 + digit;
+            }
+            return output;
+        }
+    }
+}
---
+++ ReversedNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using ReversedNumber.Models;
+
+namespace ReversedNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number:");
+            int number = int.Parse(Console.ReadLine());
+
+            int reverseNumber = Reverse.ReverseNumber(number);
+
+            Console.WriteLine($"The reverse number of entered number is {reverseNumber}");
+        }
+    }
+}
